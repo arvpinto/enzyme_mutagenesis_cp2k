b22aa9f7afd8dca62f99f18fe3912d9c08fd1480
Alexandre Pinto <up201402847@fc.up.pt>
2025-03-20 19:31:41 +0000

    Add files via upload

--- mutagenesis_scan/mut_qm_sel.sh
+++ mutagenesis_scan/mut_qm_sel.sh
@@ -4,34 +4,33 @@ shopt -s expand_aliases
 source ~/.bashrc
 
 ### Check if the usage is correct
-if [ $# -ne 4 ]; then
-    echo "Usage: ./mut_qm_sel.sh <residue_number> <residue> <topology> <qm_selection>"
+if [ $# -ne 5 ]; then
+    echo "Usage: ./mut_qm_sel.sh <residue_number> <residue> <wt_topology> <mut_topology> <qm_selection>"
     exit 1
 fi
 
 ### Variables list
 res_num="$1"
 res_type="$2"
-topology="$3"
-qm_selection="$4"
-
+wt_topology="$3"
+mut_topology="$4"
+qm_selection="$5"
 ### Set backbone and sidechain variables for QM layer check
 res_sel=""
 bb_found=true
 sc_found=true
 bb_atoms_found=()
 sc_atoms_not_found=()
-
 ### Extract backbone and sidechain information regarding the WT and mutation residues
-res_name=$(cpptraj -p ../"$topology" --resmask :"$res_num" | tail -n 1 | awk '{print $2}')
-bb_atoms_wt=$(cpptraj -p ../"$topology" --mask :"$res_num"@CA,C,O,N,H1,H2,H3,H,HA,HA2,HA3 | tail -n +2 |  awk '{print $2}')
+res_name=$(cpptraj -p ../"$wt_topology" --resmask :"$res_num" | tail -n 1 | awk '{print $2}')
+bb_atoms_wt=$(cpptraj -p ../"$wt_topology" --mask :"$res_num"@CA,C,O,N,H1,H2,H3,H,HA,HA2,HA3 | tail -n +2 |  awk '{print $2}')
 if [ "$res_name" != "GLY" ]; then
-        sc_atoms_wt=$(cpptraj -p ../"$topology" --mask ":"$res_num"&!(@CA,C,O,N,H1,H2,H3,H,HA,HA2,HA3)" | tail -n +2 |  awk '{print $2}')
+        sc_atoms_wt=$(cpptraj -p ../"$wt_topology" --mask ":"$res_num"&!(@CA,C,O,N,H1,H2,H3,H,HA,HA2,HA3)" | tail -n +2 |  awk '{print $2}')
 fi
-atom_names_wt=$(grep -o '(name[^)]*resname '"$res_name"' and resid '"$res_num"'[^)]*)' ../"$qm_selection" | sed -E 's/\(name ([^)]*)and resname '"$res_name"' and resid '"$res_num"'\)/\1/')
-bb_atoms_mut=$(cpptraj -p "$res_type"_"$res_num".prmtop --mask :"$res_num"@CA,C,O,N,H1,H2,H3,H,HA,HA2,HA3 | tail -n +2 |  awk '{print $2}')
+atom_names_wt=$(grep -o '(name[^)]*resname '"$res_name"' and resid '"$res_num"'[^)]*)' "$qm_selection" | sed -E 's/\(name ([^)]*)and resname '"$res_name"' and resid '"$res_num"'\)/\1/')
+bb_atoms_mut=$(cpptraj -p "$mut_topology" --mask :"$res_num"@CA,C,O,N,H1,H2,H3,H,HA,HA2,HA3 | tail -n +2 |  awk '{print $2}')
 if [ "$res_type" != "GLY" ]; then
-        sc_atoms_mut=$(cpptraj -p "$res_type"_"$res_num".prmtop --mask ":"$res_num"&!(@CA,C,O,N,H1,H2,H3,H,HA,HA2,HA3)" | tail -n +2 |  awk '{print $2}')
+        sc_atoms_mut=$(cpptraj -p "$mut_topology" --mask ":"$res_num"&!(@CA,C,O,N,H1,H2,H3,H,HA,HA2,HA3)" | tail -n +2 |  awk '{print $2}')
 fi
 
 ### Check if backbone is completely inserted in the QM layer
@@ -53,7 +52,7 @@ done
 
 ### Insert full mutation backbone if WT backbone is completely inserted in the QM layer
 if $bb_found; then
-
+	echo "OK BB"
         ### The backbone of GLY and PRO is considered as the full residue
         if [ "$res_type" == "GLY" ] || [ "$res_type" == "PRO" ]; then
                 res_sel="$(echo "$bb_atoms_mut" "$sc_atoms_mut")"
@@ -61,7 +60,7 @@ if $bb_found; then
                 res_sel="$(echo "$bb_atoms_mut")"
         fi
 else    ### Insert subset of atoms if backbone is incomplete in the QM layer
-
+	echo "NOT_OK_BB"
         ### GLY and PRO are handled differently to ensure a consistent QMMM layer when they are involved
         if [ "$res_type" == "GLY" ] || [ "$res_type" == "PRO" ]; then
 
@@ -82,7 +81,7 @@ fi
 
 ### Insert full mutation sidechain if WT sidechain is completely inserted in the QM layer
 if $sc_found; then
-
+	echo "OK SC"
         ### GLY and PRO have already been processed
         if [ "$res_type" == "GLY" ] || [ "$res_type" == "PRO" ]; then
                 res_sel+=""
@@ -102,7 +101,7 @@ else    ### Insert subset of atoms if sidechain is incomplete in the QM layer
 fi
 
 ### Create new $qm_selection and include the new residue
-grep -o '([^)]*)' ../"$qm_selection" | grep -v '(name[^)]*resname '"$res_name"' and resid '"$res_num"'[^)]*)' | tr '\n' ' ' | sed 's/) (/) or (/g' > $qm_selection
+grep -o '([^)]*)' "$qm_selection" | grep -v '(name[^)]*resname '"$res_name"' and resid '"$res_num"'[^)]*)' | tr '\n' ' ' | sed 's/) (/) or (/g' > temp && mv temp $qm_selection
 if [ -n "$(echo "$res_sel" | xargs)" ]; then
         echo -n "or (name "$res_sel" and resname "$res_type" and resid "$res_num")" >> $qm_selection
 fi
