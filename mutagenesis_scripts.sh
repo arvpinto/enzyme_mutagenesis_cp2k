75dec12f70d14beba3a8c578c0d1c526b57a8dc4
Alexandre Pinto <up201402847@fc.up.pt>
2025-04-01 21:52:55 +0100

    Update mutagenesis_scripts.sh

--- mutagenesis_scripts.sh
+++ mutagenesis_scripts.sh
@@ -6,7 +6,7 @@ MUT_SCAN_DIR="$PWD/mutagenesis_scan"
 MUT_MULTI_DIR="$PWD/mutagenesis_multi"
 
 ### Give executable permissions
-chmod +x "$DEL_RES_DIR/del_res_qmmm_cp2k.sh" "$DEL_RES_DIR/mol2_vmd-qmsel.sh" "$MUT_SCAN_DIR/mut_scan_qmmm_cp2k.sh" "$MUT_SCAN_DIR/sp_mutation.sh" "$MUT_SCAN_DIR/mut_qm_sel.sh" "$MUT_MULTI_DIR/mp_mut_qmmm_cp2k.sh"  "$MUT_MULTI_DIR/mp_mutation.sh"  "$MUT_MULTI_DIR/mut_qm_sel.sh"
+chmod +x "$DEL_RES_DIR/del_res_qmmm_cp2k.sh" "$DEL_RES_DIR/mol2_vmd-qmsel.sh" "$MUT_SCAN_DIR/mut_scan_qmmm_cp2k.sh" "$MUT_SCAN_DIR/sp_mutation.sh" "$MUT_SCAN_DIR/mut_qm_sel.sh" "$MUT_MULTI_DIR/mut_mp_qmmm_cp2k.sh"  "$MUT_MULTI_DIR/mp_mutation.sh"  "$MUT_MULTI_DIR/mut_qm_sel.sh"
 
 ### Export the directories to PATH
 export PATH="$DEL_RES_DIR:$PATH"
