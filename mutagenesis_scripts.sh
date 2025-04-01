1ea450198735938a40b1c651103a1c5b6a718a66
Alexandre Pinto <up201402847@fc.up.pt>
2025-04-01 21:20:11 +0100

    Update mutagenesis_scripts.sh

--- mutagenesis_scripts.sh
+++ mutagenesis_scripts.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Define the directory where your scripts are stored
+### Define the directories
 DEL_RES_DIR="$PWD/residue_deletion"
 MUT_SCAN_DIR="$PWD/mutagenesis_scan"
 MUT_MULTI_DIR="$PWD/mutagenesis_multi"
 
+### Give executable permissions
+chmod +x "$DEL_RES_DIR/del_res_qmmm_cp2k.sh" "$DEL_RES_DIR/mol2_vmd-qmsel.sh" "$MUT_SCAN_DIR/mut_scan_qmmm_cp2k.sh" "$MUT_SCAN_DIR/sp_mutation.sh" "$MUT_SCAN_DIR/mut_qm_sel.sh" "$MUT_MULTI_DIR/mp_mut_qmmm_cp2k.sh"  "$MUT_MULTI_DIR/mp_mutation.sh"  "$MUT_MULTI_DIR/mut_qm_sel.sh"
 
-
-# Export the directory to PATH if you want to run scripts without specifying the full path
+### Export the directories to PATH
 export PATH="$DEL_RES_DIR:$PATH"
 export PATH="$MUT_SCAN_DIR:$PATH"
 export PATH="$MUT_MULTI_DIR:$PATH"
