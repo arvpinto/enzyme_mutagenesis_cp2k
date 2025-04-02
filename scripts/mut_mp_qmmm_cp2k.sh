617dacea854f9b999c16a9c4b8215c2f11b3a38a
Alexandre Pinto <up201402847@fc.up.pt>
2025-04-02 18:23:46 +0100

    Update mut_mp_qmmm_cp2k.sh

--- scripts/mut_mp_qmmm_cp2k.sh
+++ scripts/mut_mp_qmmm_cp2k.sh
@@ -157,8 +157,8 @@ for resid in $(awk '{print $1}' "$mut_list"); do
         sed -i 's/PROJECT.*/PROJECT MUT_SCAN_OPT_'"$ts_structure"'/' opt_res_"$ts_structure".inp 
         sed -i 's/PROJECT.*/PROJECT MUT_SCAN_SP_'"$r_structure"'/' sp_res_"$r_structure".inp
         sed -i 's/PROJECT.*/PROJECT MUT_SCAN_SP_'"$ts_structure"'/' sp_res_"$ts_structure".inp
-        sed -i 's/COORD_FILE_NAME.*/COORD_FILE_NAME '"$resid"'_'"$r_structure"'.rst7/g' opt_md_res_"$r_structure".inp
-        sed -i 's/COORD_FILE_NAME.*/COORD_FILE_NAME '"$resid"'_'"$ts_structure"'.rst7/g' opt_md_res_"$ts_structure".inp
+        sed -i 's/COORD_FILE_NAME.*/COORD_FILE_NAME '"$resid"'_'"$r_structure"'.pdb/g' opt_md_res_"$r_structure".inp
+        sed -i 's/COORD_FILE_NAME.*/COORD_FILE_NAME '"$resid"'_'"$ts_structure"'.pdb/g' opt_md_res_"$ts_structure".inp
 	sed -i 's/PARM_FILE_NAME.*/PARM_FILE_NAME '"$resid"'.prmtop/g' *_res_*.inp
 	sed -i 's/CONN_FILE_NAME.*/CONN_FILE_NAME '"$resid"'.prmtop/g' *_res_*.inp
 	sed -i 's/MUT_SCAN/MUT_SCAN_OPT_MD_'"$r_structure"'-1.restart/' md_extrest_"$r_structure".inc
